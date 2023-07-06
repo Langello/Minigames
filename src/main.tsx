7f0cc0d5f3a379a2722d495c3f1e3d0e1326691e
Langello <gastonchavo@gmail.com>
2023-07-05 21:40:39 -0300

    hasrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
 )
