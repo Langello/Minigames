f9248b5b4c0ed7a52958abc8c6fb721a94282497
Langello <gastonchavo@gmail.com>
2023-07-05 21:46:24 -0300

    hasrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
